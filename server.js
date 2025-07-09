4e58817dbaf0577f5575d6771d7b0b1650ed9d8d
Austin9tech <augsteenstorez@gmail.com>
2025-07-09 10:14:07 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,9 +1,8 @@
 const express = require('express');
 const cors = require('cors');
-const { path: pathToYtDlp } = require('yt-dlp-exec');
 const YTDlpWrap = require('yt-dlp-wrap').default;
 
-const ytdlpWrap = new YTDlpWrap(pathToYtDlp);
+const ytdlpWrap = new YTDlpWrap('./yt-dlp');
 const app = express();
 
 app.use(cors());
